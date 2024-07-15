b169b60b7f4cfe138d0364dd4d8b3ca58d8a806e
moisesfdasilva <msesfernandes@gmail.com>
2024-07-15 19:35:06 -0300

    refactor: change style folder for a single file, style.css.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   include_once "models/Page_Data.class.php";
   $pageData = new Page_Data();
   $pageData->setTitle("Futevôlei P10");
-  $pageData->setCss("<link rel='stylesheet' href='style'>");
+  $pageData->setCss("<link rel='stylesheet' href='style.css'>");
   $pageData->setContent($nav);
   // include_once "models/database.php";
   include_once "controllers/home.php";
