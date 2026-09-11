37a2f5c489bc0c42d04ba18f536f40bfe068d59b
MFSilva <msesfernandes@gmail.com>
2026-09-11 12:39:06 -0300

    feat: create a standard page with header/nav, main, and footer.

--- controllers/contact.php
+++
@@ -1,6 +0,0 @@
-<?php
-  include_once "views/header-html.php";
-  include_once "views/menu-nav-html.php";
-  include_once "views/contact-html.php";
-  include_once "views/footer-html.php";
-?>
--- controllers/tournament.php
+++
@@ -1,6 +0,0 @@
-<?php
-  include_once "views/header-html.php";
-  include_once "views/menu-nav-html.php";
-  include_once "views/tournament-html.php";
-  include_once "views/footer-html.php";
-?>
